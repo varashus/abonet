bfb88a05861fd3481aae40f07b8e358cfc8c9c9c
MenőManó <MenMan@DESKTOP-EKDAK0U>
2023-09-19 21:40:13 +0200

    Add project files.

---
+++ ConsoleApp3/Abonet.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    class Abonet
+    {
+
+
+        public string nev { get; set; }
+        public double zsir { get; set; }
+        public double szenhidrat { get; set; }
+
+
+
+        public Abonet(string sor)
+        {
+            string[] r = sor.Split(';');
+            this.nev = r[0];
+            this.zsir = double.Parse(r[1]);
+            this.szenhidrat = double.Parse(r[2]);
+        }
+
+
+    }
+}
---
+++ ConsoleApp3/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Text.Unicode;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            var abonetek = new List<Abonet>();
+            using var sr = new StreamReader(
+                path: @"..\..\..\src\abi.txt",
+                encoding: Encoding.UTF8
+
+                );
+            
+            
+            while (!sr.EndOfStream)
+            {
+                abonetek.Add(new Abonet(sr.ReadLine()));
+            }
+            foreach (var x in abonetek)
+            {
+                
+                Console.WriteLine($"{x.nev}  {x.zsir}  {x.szenhidrat}");
+
+            }
+
+
+            Console.WriteLine();
+            double legkisebbzsir = abonetek.Min(abonetek => abonetek.zsir);
+            foreach (var x in abonetek)
+            {
+                
+                if (x.zsir == legkisebbzsir)
+                {
+                    Console.WriteLine(x.nev);
+                }
+            }
+            Console.WriteLine();
+            Console.ReadLine();
+
+        }
+    }
+}
